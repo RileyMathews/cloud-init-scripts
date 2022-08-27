cca46f0cad50a67c348e95336483ad424d1aa2ed
Riley Mathews <rileymathews80@gmail.com>
2022-08-27 15:08:35 -0500

    add sudo where needed

--- debian/python_install.sh
+++ debian/python_install.sh
@@ -3,11 +3,11 @@
 PYTHON_VERSION=3.10.6
 PYTHON_MINOR=3.10
 
-apt update -y
-apt upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 
 # install python
-apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 
 cd /tmp
 wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
